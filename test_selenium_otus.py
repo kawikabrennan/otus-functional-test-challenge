cace6616a9d1c30881c3e24cba8fd0f1edb17bc1
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-11-09 14:07:28 -0800

    Renamed the file and converted to a unit test
    python -m unittest test_selenium_otus.py
    that works now

---
+++ test_selenium_otus.py
@@ -0,0 +1,27 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class ChromeSearch(unittest.TestCase):
+
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.driver = webdriver.Chrome(chrome_options=options)
+
+    def test_search_in_python_org(self):
+        self.driver.get("http://www.python.org")
+        assert "Python" in self.driver.title
+        elem = self.driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in self.driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
