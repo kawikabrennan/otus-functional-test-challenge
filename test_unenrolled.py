348656b0561ff1f8bd2d225e1151218ebedc3909
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-11-10 11:00:23 -0800

    Completed the test for adding a link
    I've added a wait since sometimes the test is failing, which appears due to elements taking longer than expected to load. There needs to be some cleanup.

--- test_unenrolled.py
+++ test_unenrolled.py
@@ -3,13 +3,16 @@ from credentials import USERDEFAULT, PASSDEFAULT
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 
 class UnenrolledTests(unittest.TestCase):
     @classmethod
     def otus_login(cls):
-        cls.driver.get("https://my.otus.com/")
+        cls.driver.get(cls.otus_home_page)
         cls.driver.find_element_by_id(
             "otus-input-1").send_keys(USERDEFAULT)
         cls.driver.find_element_by_id(
@@ -18,6 +21,9 @@ class UnenrolledTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls.otus_home_page = "https://my.otus.com/"
+        cls.otus_my_bookshelf = cls.otus_home_page + "bookshelf/my-bookshelf"
+
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
         cls.driver = webdriver.Chrome()  # (chrome_options=options)
@@ -26,13 +32,19 @@ class UnenrolledTests(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # sleep(3)
+        sleep(3)
         cls.driver.close()
 
     def test_assessments_none_available(self):
         """The Assessments table is empty."""
         self.driver.find_element_by_xpath(
             "//a/span[.='Assessments']").click()
+
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, "otus-large-table"))
+        )
+
         assessments_table = self.driver.find_elements_by_xpath(
             "//table/tbody/*"
         )
@@ -41,18 +53,43 @@ class UnenrolledTests(unittest.TestCase):
 
     def test_bookshelf_add_link(self):
         """A link is added to the resource table."""
+        link_name = "Log In URL"
 
+        # TODO: Work on hover action instead of going directly to the page
         # element_to_hover_over = self.driver.find_element_by_xpath(
         #     "//ot-student-navbar/div/div")
         # hover = ActionChains(self.driver).move_to_element(
         #     element_to_hover_over)
         # hover.perform()
 
-        self.driver.get("https://my.otus.com/bookshelf/my-bookshelf")
+        self.driver.get(self.otus_my_bookshelf)
         self.driver.find_element_by_xpath(
             "//button[@aria-label='open actions menu']").click()
 
-        # self.assertEqual(len(assessments_table), 0)
+        menu_items = self.driver.find_elements_by_class_name(
+            "ot-option-container")
+        menu_items[0].click()
+
+        self.driver.find_element_by_xpath(
+            "//div/span[.='Link']").click()
+
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, "otus-new-modal__wrapper"))
+        )
+
+        text_fields = self.driver.find_elements_by_xpath(
+            "//div[@class='otus-new-modal__wrapper']//input[@type='text']")
+        text_fields[0].send_keys(self.otus_home_page)
+        text_fields[1].send_keys(link_name)
+
+        self.driver.find_element_by_xpath(
+            "//button/span[contains(.,'Save')]").click()
+
+        created_link = self.driver.find_element_by_xpath(
+            f'//table//span[.="{link_name}"]')
+
+        self.assertTrue(created_link)
 
 
 if __name__ == "__main__":
