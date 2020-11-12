f657c6191a34c323c0e37c8bf04e2bcbf7761717
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-11-12 12:23:00 -0800

    Comments for credentials

--- credentials.py
+++ credentials.py
@@ -1,4 +1,9 @@
+# Email for student enrolled in the interview class "QA Tech Challenge"
 USERENROLLED = ""
+# Password
 PASSENROLLED = ""
+
+# Email for student not enrolled in any classes.
 USERDEFAULT = ""
+# Password
 PASSDEFAULT = ""
